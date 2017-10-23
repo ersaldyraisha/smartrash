1b84ba48ae6cc7237f27036731b4816ada186cd2
Andi Ersaldy Raisha <andi.ersaldy@gmail.com>
2017-10-23 13:05:56 +0700

    Initial Commit
    - init android project

---
+++ app/src/main/java/com/ersaldyraisha/smartrash/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ersaldyraisha.smartrash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
